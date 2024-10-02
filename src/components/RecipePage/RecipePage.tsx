cf52238f72169a5522dde41706424e3f5ab2df42
SoleneOclock <solene@oclock.io>
2024-10-02 11:42:43 +0200

    mise en place des link et des routes et creation du composant RecipePage qui fetch la recette dont le slug se trouve dans l'url (pour le recuperer on a utilisé useParams)

--- src/components/RecipePage/RecipePage.tsx
+++ src/components/RecipePage/RecipePage.tsx
@@ -1,5 +1,9 @@
 import { useParams } from 'react-router-dom';
 import IRecipe from '../../@types/recipe';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import './RecipePage.scss';
 
 interface IRecipePageProps {
 	recipes: IRecipe[];
@@ -14,31 +18,56 @@ export default function RecipePage({ recipes }: IRecipePageProps) {
 	// on a recup le slug mais on veut l'objet recette associé à ce slug
 	// - soit on fetch l'API pour recuperer la recette
 	// - soit on recup la listed es recettes en props depuis app et on find celle qu'on veut
-	const recipeToDisplay = recipes.find((recette) => recette.slug === slug);
-	console.log(recipeToDisplay);
+	const [recipeToDisplay, setRecipeToDisplay] = useState<null | IRecipe>(null);
+
+	useEffect(() => {
+		const fetchRecipeFromSlug = async () => {
+			try {
+				const response = await axios.get(
+					`https://orecipesapi.onrender.com/api/recipes/${slug}`,
+				);
+				console.log(response);
+				// on place la recette reçue dans le state pour l'afficher
+				setRecipeToDisplay(response.data);
+			} catch (e) {
+				console.log(e);
+			}
+		};
+		fetchRecipeFromSlug();
+	}, [slug]);
 
 	if (!recipeToDisplay) {
-		return <div>Erreur recette non trouvée</div>;
+		return <div>...</div>;
 	}
 
 	return (
 		<main className="Recipe">
-			<h1>{recipeToDisplay.title}</h1>
-			<img
-				className="Recipe-img"
-				src={recipeToDisplay.thumbnail}
-				alt={recipeToDisplay.title}
-			/>
+			<div className="Recipe__header">
+				<img
+					className="Recipe__img"
+					src={recipeToDisplay.thumbnail}
+					alt={recipeToDisplay.title}
+				/>
+				<div className="Recipe__infos">
+					<h1>{recipeToDisplay.title}</h1>
+					<p>Difficlutée : {recipeToDisplay.difficulty}</p>
+				</div>
+			</div>
 
-			<ul className="Recipe-ingredients">
+			<ul className="ingredients">
 				{recipeToDisplay.ingredients.map((ingredient) => (
-					<li key={ingredient.id}>{ingredient.name}</li>
+					<li key={ingredient.id} className="ingredients__item">
+						<span className="ingredients__quantity">{ingredient.quantity}</span>
+						{ingredient.name}
+					</li>
 				))}
 			</ul>
 
-			<ul className="Recipe-step">
+			<ul className="step">
 				{recipeToDisplay.instructions.map((step) => (
-					<li key={step}>{step}</li>
+					<li key={step} className="step__item">
+						{step}
+					</li>
 				))}
 			</ul>
 		</main>
